12ab0a8d68e4754e6acd5be378c9d0bf4bd62242
Zoren <zorenleolumosbog@gmail.com>
2023-02-06 20:54:03 +0800

    commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<div class="page-header-inner" >
+    <div class="page-header-inner" >
+        <div class="navbar-header">
+            <a href="{{ url('/') }}"
+               class="navbar-brand">
+                Time Sheet System
+            </a>
+        </div>
+        <a href="javascript:;"
+           class="menu-toggler responsive-toggler"
+           data-toggle="collapse"
+           data-target=".navbar-collapse">
+        </a>
+
+         <div class="top-menu">
+            <ul class="nav navbar-nav pull-right">
+                <li>
+                    <a href="#" target="_blank">v1.0</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
